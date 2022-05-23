a59a5988f16e179fec7e308172bcfdbc003be769
Lin zihan <893833413@qq.com>
2022-05-23 19:57:43 +0800

    Add Autotest for SSCDR

--- tests/test_model.py
+++ tests/test_model.py
@@ -61,6 +61,13 @@ class TestRecommender(unittest.TestCase):
         }
         quick_test(config_dict)
 
+    def test_sscdr(self):
+        config_dict = {
+            'model': 'SSCDR',
+            'train_epochs': ["SOURCE:1", "TARGET:1", "BOTH:1"],
+        }
+        quick_test(config_dict)
+
 
 if __name__ == '__main__':
     unittest.main()
