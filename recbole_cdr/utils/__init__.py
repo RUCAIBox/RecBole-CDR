c9a8304b938d34cbbb77a8dbbc0d72c76683e24b
Wicknight <61172796+Wicknight@users.noreply.github.com>
2023-05-13 10:42:46 +0800

    Update __init__.py

--- recbole_cdr/utils/__init__.py
+++ recbole_cdr/utils/__init__.py
@@ -1,6 +1,6 @@
-from recbole_cdr.utils.utils import get_model, get_trainer
+from recbole_cdr.utils.utils import get_model, get_trainer, get_keys_from_chainmap_by_order
 from recbole_cdr.utils.enum_type import *
 
 __all__ = [
-    'get_model', 'get_trainer', 'Enum', 'ModelType', 'CrossDomainDataLoaderState', 'train_mode2state'
+    'get_model', 'get_trainer', 'Enum', 'ModelType', 'CrossDomainDataLoaderState', 'train_mode2state', 'get_keys_from_chainmap_by_order'
 ]
